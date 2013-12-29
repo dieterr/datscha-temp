82512cfefbf09d610a1655263d020c5bb5acc51b
Dieter Riegler <dieter.riegler@gmail.com>
2013-12-29 10:42:55 +0100

    basic documentation done

--- temp.py
+++ temp.py
@@ -1,18 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+## this scrpit is based on http://pastebin.com/aBjEPPq0
+
 import os
 import glob
 import time
 import datetime
 
+## starting the kernel-modules
+## added this to /etc/modules, so most likely not necessary here
+## testing this next time
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
+## setting the corect files of the sensors
 base_dir = '/sys/bus/w1/devices/'
-#device_folder1 = glob.glob(base_dir + '10-000802b54*')[0]
-#device_folder2 = glob.glob(base_dir + '10-000802b58*')[0]
 device_folder = glob.glob('/sys/bus/w1/devices/10*')
 device_file = [device_folder[0] + '/w1_slave', device_folder[1] + '/w1_slave']
 
-
+## reading actuall temperature of two sensors
 def read_temp_raw():
     f1 = open(device_file[0], 'r')
     lines1 = f1.readlines()
@@ -22,6 +29,7 @@ def read_temp_raw():
     f2.close()
     return lines1 + lines2
 
+## extracting needed information
 def read_temp():
     lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES' or lines[2].strip()[-3:] != 'YES':
@@ -32,21 +40,8 @@ def read_temp():
     temp2 = float(lines[3][equals_pos[1]+2:])/1000
     timestamp = time.strftime("%y-%m-%d %H:%M:%S")
     return timestamp, temp1, temp2
-    
-#print read_temp()
-#time.sleep(60)
-#time.sleep(900)
 
+## write results into file
 fobj = open("/home/pi/temp/temp-datscha2S.txt", "a")
 fobj.write(str(read_temp()[0]) + ";" + str(read_temp()[1]) + ";" + str(read_temp()[2]) + '\n')
 fobj.close()
-
-
-##original
-#while True:
-#    #print read_temp()
-#    fobj = open("/tmp/temp-datscha.txt", "a")
-#    fobj.write(str(read_temp()[0]) + ";" + str(read_temp()[1]) + ";" + str(read_temp()[2]) + ";" + '\n')
-#    fobj.close()
-#    #time.sleep(60)
-#    #time.sleep(900)#
